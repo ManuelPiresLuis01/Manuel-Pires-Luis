7c26d7e911cecd7e07bd65fb890c14c2d2d8486b
Manuel Pires Luis <mpl31052001@gmail.com>
2024-05-28 15:37:01 +0100

    primeiro commit do meu portfolio

---
+++ Assets/script/mainScript.js
@@ -0,0 +1,19 @@
+let menu = document.getElementById("menu")
+let botaoMenu = document.getElementById("btn-menu")
+let overflow = document.querySelector(".overflow")
+
+botaoMenu.addEventListener("click",function(){
+    menu.classList.add("abrir")
+    overflow.classList.add("abrir")
+})
+
+menu.addEventListener("click",function(){
+    menu.classList.remove("abrir")
+    overflow.classList.remove("abrir")
+})
+
+
+overflow.addEventListener("click",function(){
+    menu.classList.remove("abrir")
+    overflow.classList.remove("abrir")
+})
