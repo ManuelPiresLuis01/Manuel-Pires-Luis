df924ec8014273b1bea970a17f174cfb6a6d78b6
Manuel Pires Luis <mpl31052001@gmail.com>
2024-05-31 14:35:48 +0100

    comitando - sofri tentando achar o bug que eu mesmo criei

--- Assets/script/mainScript.js
+++ Assets/script/mainScript.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let menu = document.getElementById("menu")
 let botaoMenu = document.getElementById("btn-menu")
 let overflow = document.querySelector(".overflow")
@@ -21,27 +20,3 @@ overflow.addEventListener("click",function(){
 
 
 
-=======
-let menu = document.getElementById("menu")
-let botaoMenu = document.getElementById("btn-menu")
-let overflow = document.querySelector(".overflow")
-
-botaoMenu.addEventListener("click",function(){
-    menu.classList.add("abrir")
-    overflow.classList.add("abrir")
-})
-
-menu.addEventListener("click",function(){
-    menu.classList.remove("abrir")
-    overflow.classList.remove("abrir")
-})
-
-
-overflow.addEventListener("click",function(){
-    menu.classList.remove("abrir")
-    overflow.classList.remove("abrir")
-})
-
-
-
->>>>>>> 42076c3fb0ca913f8ecc5d9a60389be88164836c
