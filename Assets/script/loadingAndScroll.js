5938e0313c1acbc356f44a15e6756bea468b67e1
Manuel Pires Luis <166626626+ManuelPiresLuis01@users.noreply.github.com>
2024-09-12 18:10:51 +0100

    Update loadingAndScroll.js

--- Assets/script/loadingAndScroll.js
+++ Assets/script/loadingAndScroll.js
@@ -10,7 +10,7 @@ parar = setInterval(function () {
         first()
     }
 
-},1)
+},100)
 window.revelar = ScrollReveal({ reset: true })
 function first() {/*
     revelar.reveal('.img', {
@@ -18,26 +18,22 @@ function first() {/*
         distance: '90px'
     })*/
 
-    
     revelar.reveal('#presentation1', {
         duration: 500,
         distance: '90px',
         delay: 200
     })
-
     revelar.reveal('#presentation', {
         duration: 500,
         distance: '90px',
         delay: 250
     })
-
     revelar.reveal('.r1', {
         duration: 200,
         distance: '90px',
         delay: 500,
         origin: 'right'
     })
-
     revelar.reveal('.r2', {
         duration: 200,
         distance: '90px',
@@ -62,7 +58,6 @@ function first() {/*
         delay: 900,
         origin: 'right'
     })
-
     revelar.reveal('.btn', {
         duration: 200,
         distance: '90px',
@@ -75,13 +70,11 @@ revelar.reveal('.about-me', {
     duration: 500,
     distance: '50px',
 })
-
 revelar.reveal('.photo', {
     duration: 500,
     distance: '50px',
     delay:100
 })
-
 revelar.reveal('.bxl-html5', {
     duration: 500,
     distance: '50px',
@@ -107,13 +100,11 @@ revelar.reveal('.bxl-bootstrap', {
     distance: '50px',
     delay: 500
 })
-
 revelar.reveal('.bxl-php', {
     duration: 500,
     distance: '50px',
     delay: 550
 })
-
 revelar.reveal('.bxl-react', {
     duration: 500,
     distance: '50px',
@@ -124,33 +115,27 @@ revelar.reveal('.bxl-visual-studio', {
     distance: '50px',
     delay: 700
 })
-
 revelar.reveal('.bi-file-word', {
     duration: 500,
     distance: '50px',
     delay: 800
 })
-
 revelar.reveal('.bi-file-earmark-excel-fill', {
     duration: 500,
     distance: '50px',
     delay: 900
 })
-
 revelar.reveal('.bi-file-ppt-fill', {
     duration: 500,
     distance: '50px',
     delay: 1000
 })
-
 /*efeito scroll reveal nos titulos*/
 
 revelar.reveal('.titulo', {
     duration: 500,
     distance: '90px'
 })
-
-
 /*efeito scroll reveal em oque faço*/
 revelar.reveal('.oque-faco1', {
     duration: 300,
@@ -167,7 +152,6 @@ revelar.reveal('.oque-faco3', {
     distance: '90px',
     delay: 300
 })
-
 /**efeito scroll reveal dos projectos */
 revelar.reveal('.proj1', {
     duration: 300,
@@ -184,14 +168,12 @@ revelar.reveal('.proj3', {
     distance: '90px',
     delay: 300
 })
-
 /**efeito scroll reveal do projecto premiado */
 revelar.reveal('.projp1', {
     duration: 300,
     distance: '90px',
     delay: 100
 })
-
 /**efeito scroll reveal das testemunhas */
 revelar.reveal('.testemunha1', {
     duration: 300,
@@ -213,40 +195,31 @@ revelar.reveal('.testemunha4', {
     distance: '90px',
     delay: 400
 })
-
-
 revelar.reveal('.container', {
     duration: 300,
     distance: '90px',
     delay: 500
 })
-
-   
-
 revelar.reveal('.bxs-envelope', {
     duration: 300,
     distance: '90px',
     delay: 550
 })
-
 revelar.reveal('.nome', {
     duration: 300,
     distance: '90px',
     delay: 600
 })
-
 revelar.reveal('.email', {
     duration: 300,
     distance: '90px',
     delay: 650
 })
-
 revelar.reveal('.msg', {
     duration: 300,
     distance: '90px',
     delay: 700
 })
-
 revelar.reveal('.enviar', {
     duration: 300,
     distance: '90px',
